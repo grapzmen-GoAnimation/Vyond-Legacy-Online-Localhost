fe610d9bb1a37db69f78da4a0d552bc03a9602e8
grapzmen-GoAnimation <arthurhenriqueesr@gmail.com>
2025-09-26 14:00:51 -0300

    Add files via upload

---
+++ faststart.bat
@@ -0,0 +1,32 @@
+:: Important stuff
+@echo off && cls
+title Vyond Legacy Online
+if not exist "installed" (if not exist "notinstalled" (echo Vyond Legacy Online Configuration File >> notinstalled))
+::::::::::::::::::::
+:: Initialization ::
+::::::::::::::::::::
+
+:: Terminate existing node.js apps
+TASKKILL /IM node.exe /F 2>nul
+cls
+
+:::::::::::::::::::::::::::::
+:: Start GoAnimate Remastered ::
+:::::::::::::::::::::::::::::
+
+:: Check for installation
+if exist notinstalled (
+	echo Vyond Legacy Online is not installed! Installing...
+	call npm install
+	ren "notinstalled" "installed"
+	cls
+	goto start
+) else (
+	goto start
+)
+
+:: Run npm start
+:start
+echo Vyond Legacy Online is now starting...
+echo Please Navigate To http://localhost On Your Browser.
+npm start
